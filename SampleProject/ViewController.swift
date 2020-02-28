dc0a28ede8678a206751eb9143014eda05efe4e7
Eugene Smolyakov <jendby@gmail.com>
2020-02-28 13:36:21 +0400

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Eugene Smolyakov on 28/02/2020.
+//  Copyright © 2020 ES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
